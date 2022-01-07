6721254ee9279df928dd26a58b9fa5787f782be7
emmaneilson <eneilso@uwo.ca>
2022-01-06 22:03:33 -0700

    added the current geo-location of the user to appear as a pin on the map. User must accept permissions when prompted to see their current location.
    To make the location work, need to open up google maps to create a "last known location" before debugging our app. Can create own pin in the ... settings of the simulator. Right now its set to california.

--- app/src/main/java/com/example/safetravels/MapsActivity.kt
+++ app/src/main/java/com/example/safetravels/MapsActivity.kt
@@ -1,7 +1,16 @@
 package com.example.safetravels
 
+import android.content.ContentProviderClient
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.LocationManager
+import android.location.LocationRequest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.location.LocationManagerCompat.isLocationEnabled
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -10,11 +19,18 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.example.safetravels.databinding.ActivityMapsBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import java.util.jar.Manifest
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+    private var userPos = LatLng(-200.0, 151.0)
+    private var PERMISSION_ID = 1234
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    //lateinit var locationRequest: LocationRequest
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,10 +38,33 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        getLastLocation()
+    }
+
+    private fun getLastLocation(){
+        if(checkPermission()){
+            if(isLocationEnabled()){
+                fusedLocationProviderClient.lastLocation.addOnCompleteListener { task ->
+                    var location = task.result
+
+                    if(location == null){
+
+                    }else{
+                        userPos = LatLng(location.latitude, location.longitude)
+                        onMapReady(mMap)
+                    }
+                }
+            }else{
+                Toast.makeText(this, "Please enable your Location Service!", Toast.LENGTH_LONG).show()
+            }
+        }else{
+            requestPermission()
+        }
     }
 
     /**
@@ -40,9 +79,44 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        mMap.addMarker(MarkerOptions().position(userPos).title("User Current Location"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(userPos))
+    }
+
+    private fun checkPermission():Boolean{
+        if(
+            ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
+            ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+        ){
+            return true
+        }
+        return false
+    }
+
+    private fun requestPermission(){
+
+        ActivityCompat.requestPermissions(
+          this,
+           arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION), PERMISSION_ID
+        )
+    }
+
+    private fun isLocationEnabled():Boolean{
+        var locationManager:LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if(requestCode == PERMISSION_ID){
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Log.d("Debug", "permission granted")
+                getLastLocation()
+            }
+        }
     }
 }
